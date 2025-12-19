f1660543d3db950d7fd5361ac1c16776dc58d975
ttqf <975414890@qq.com>
2025-12-19 19:43:13 +0800

    更新 AI 配置、5.2 功能限制

--- src/stores/limitaions.ts
+++ src/stores/limitaions.ts
@@ -6,13 +6,13 @@ export function getLimitaion(type: 'maxMediaDuration' | 'maxWorkingDuration' | '
 		case 'maxWorkingDuration':
 			return n < 45 ? 671 : undefined;
 		case 'maxUploadSizeGB':
-			return n < 15 ? 1 : n < 45 ? 4 : n < 55 ? 10 : undefined;
+			return n < 15 ? 1 : n < 30 ? 4 : n < 45 ? 10 : n < 65 ? 32 : 1024;
 		case 'maxTaskListCount':
-			return n < 20 ? 20 : n < 40 ? 66 : n < 60 ? 99 : undefined;
+			return n < 20 ? 20 : n < 55 ? 99 : n < 70 ? 256 : 5000;
 		case 'maxThreads':
-			return n < 20 ? 4 : n < 35 ? 6 : n < 55 ? 9 : n < 70 ? 99 : undefined;
+			return n < 20 ? 4 : n < 35 ? 6 : n < 50 ? 9 : n < 70 ? 99 : 256;
 		case 'maxFilterNodeCount':
-			return n < 20 ? 20 : n < 40 ? 66 : n < 60 ? 99 : undefined;
+			return n < 20 ? 20 : n < 40 ? 66 : n < 60 ? 99 : 999;
 		default:
 			break;
 	}
